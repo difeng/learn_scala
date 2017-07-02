6de09b7e25eef45949c732ebc9ae9dde5f3f725e
difeng <difengs@outlook.com>
2017-07-02 18:23:39 +0800

    chapter8

---
+++ src/main/scala/chapter5/chapter5.scala
@@ -0,0 +1,13 @@
+package chapter5
+
+/**
+  * Created by difeng on 2017/7/1.
+  * 类中的字段自动带有getter方法和setter方法。
+  * 你可以用定制的getter/setter方法替换掉字段的定义，而不必修改使用类的客户端-这就是所谓的“统一访问原则”
+  * 用@BeanProperty注解来生成JavaBeans的getXxx/setXxx方法
+  * 每个类都有一个组要点额构造器，这个构造器和类定义“交织”在一起。它的参数直接成为类的字段。主构造器执行类体中所有的语句
+  * 辅助构造器是可选的。它们叫做this。
+  */
+object chapter5 {
+
+}
