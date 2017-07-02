d757a51b8531976eb7bf76b248f7883af6cba904
difeng <difengs@outlook.com>
2017-07-02 18:23:21 +0800

    chapter8

--- src/main/scala/chapter6/chapter6.scala
+++ src/main/scala/chapter6/chapter6.scala
@@ -2,94 +2,14 @@ package chapter6
 
 /**
   * Created by difeng on 2017/6/27.
+  *
+  * 用对象作为单例或存放工具方法
+  * 类可以拥有一个同名的伴生对象
+  * 对象可以扩展类或特质
+  * 对象的apply方法通常用来构造伴生类的新实例
+  * 如果不想显式定义main方法，可以用扩展App特质的对象
+  * 你可以通过扩展Enumeration对象来实现枚举
   */
 object chapter6 {
 
-  def exercise1(): Unit = {
-    object Conversions {
-      def inchesToCentimeters(): Unit = {}
-      def gallonsToLiters(): Unit = {}
-      def milesTokilometers(): Unit = {}
-    }
-  }
-
-  def exercise2(): Unit = {
-    abstract class UnitConversion {
-      def inchesToCentimeters() {}
-      def gallonsToLiters() {}
-      def milesTokilometers() {}
-    }
-
-    object InchesToCentimeters extends UnitConversion {
-      override def inchesToCentimeters() = {}
-    }
-
-    object GallonsToLiters extends UnitConversion {
-      override def gallonsToLiters() = {}
-    }
-
-    object MilesToKilometers extends UnitConversion {
-      override def milesTokilometers() = {}
-    }
-
-  }
-
-  def exercise3 (): Unit = {
-    object origin extends java.awt.Point {
-
-    }
-    println(origin)
-  }
-
-  def exercise5(): Unit = {
-
-  }
-
-  def exercise6(): Unit = {
-    object Card extends Enumeration {
-      val club =  Value("♣")
-      val diamond = Value("♦")
-      val spade = Value("♥")
-      val heart = Value("♠")
-    }
-    for(card <- Card.values) { println(card.toString + " ")}
-  }
-
-  def exercise7(): Unit = {
-    object Card extends Enumeration {
-      val club =  Value("♣")
-      val diamond = Value("♦")
-      val spade = Value("♥")
-      val heart = Value("♠")
-
-      def isSpade(card: Card.Value): Boolean = {
-        card == spade || card == diamond
-      }
-    }
-    println(Card.isSpade(Card.spade))
-  }
-
-  def exercise8(): Unit = {
-    object RGB extends Enumeration {
-      val RED = Value(0xff0000,"RED")
-      val BLACK = Value(0x000000,"BLACK")
-      val GREEN = Value(0x00ff00,"GREEN")
-      val CYAN = Value(0x00ffff,"CYAN")
-      val YELLOW = Value(0xffff00,"YELLOW")
-      val WHITE = Value(0xffffff,"WHITE")
-      val BLUE = Value(0x0000ff,"BLUE")
-      val MAGENTA = Value(0xff00ff,"MAGENTA")
-    }
-    for(c <- RGB.values) println(c.id + ":" + c)
-  }
-
-  def main(args: Array[String]): Unit = {
-    exercise1()
-    exercise2()
-    exercise3()
-    exercise5()
-    exercise6()
-    exercise7()
-    exercise8()
-  }
 }
