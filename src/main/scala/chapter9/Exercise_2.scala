f5a69602a81a7dfe14a4e1d61609d843d7217c0f
difeng <difengs@outlook.com>
2017-07-04 15:39:43 +0800

    fix align

--- src/main/scala/chapter9/Exercise_2.scala
+++ src/main/scala/chapter9/Exercise_2.scala
@@ -12,7 +12,7 @@ import scala.io.Source
 object Exercise_2 extends App {
   //write a file
   val out = new PrintWriter("src/main/scala/chapter9/tables.txt")
-  for (i <- 1 to 10) {
+  for (i <- 1 to 100) {
     val cells = Array("a","b","c")
     val line = cells.map(x => x + i).mkString("\t")
     out.println(line)
@@ -20,11 +20,12 @@ object Exercise_2 extends App {
   out.close()
 
   //reverse the file
-//  val source = Source.fromFile("src/main/scala/chapter9/tables.txt")
-//  val arr = source.getLines.toArray
-//  val out1 = new PrintWriter("src/main/scala/chapter9/tables.txt")
-//  for (line <- arr) {
-//    out1.println(line.replaceAll("\t"," "))
-//  }
-//  out1.close()
+  val source = Source.fromFile("src/main/scala/chapter9/tables.txt")
+  val arr = source.getLines.toArray
+  val out1 = new PrintWriter("src/main/scala/chapter9/tables.txt")
+  for (line <- arr) {
+    val newLine = line.split("\t").map(x => x + " "*(12 - x.length)).mkString
+    out1.println(newLine)
+  }
+  out1.close()
 }
